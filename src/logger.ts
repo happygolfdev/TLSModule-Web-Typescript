215100ac0637ed370ed28ed82c9f47f05b8b2840
Justin Ji <jjeui0308@gmail.com>
2020-01-19 13:23:14 +0900

    comments added

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,19 @@
+/**
+ * @summary 콘솔에 로그를 출력하는 클래스
+ */
 class Logger {
+  /**
+   * @summmary 에러 로그를 출력한다
+   * @param message 에러 메세지 내용
+   */
   static showError(message: String) {
     console.log(`${Date()}: 😱  ${message}`);
   }
 
+  /**
+   * @summary 일반적인 로그 메세지를 출력한다
+   * @param message 메세지 내용
+   */
   static showMessage(message: String) {
     console.log(`${Date()}: 😎  ${message}`);
   }
--- src/userManager.ts
+++ src/userManager.ts
@@ -1,8 +1,14 @@
 import { User } from "./user";
 
+/**
+ * @summary 클라이언트에서 사용자 데이터를 로컬 스토리지에 저장하고 관리하는 매니져 클래스
+ */
 class UserManager {
   private key: String = "tls_client";
 
+  /**
+   * @summary 저장되어 있는 사용자 데이터를 받아온다
+   */
   public getUser() {
     const dataString = window.localStorage.getItem(String(this.key));
     if (dataString == null) {
@@ -12,6 +18,10 @@ class UserManager {
     return JSON.parse(dataString);
   }
 
+  /**
+   * @summary 사용자 데이터를 로컬스토리지에 저장한다.
+   * @param user 사용자 데이터의 Object
+   */
   public setUser(user: any) {
     const userString = JSON.stringify(user);
     window.localStorage.setItem("tls_client", userString);
